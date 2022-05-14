17723fc3a86df54cdad2156a4ccc54aa5ac62f64
Sebastian Fichtner <contact@flowtoolz.com>
2022-05-14 23:58:11 +0100

    Add symbol kind name

--- Sources/SwiftLSP/Use Cases/Symbols/LSPDocumentSymbol.swift
+++ Sources/SwiftLSP/Use Cases/Symbols/LSPDocumentSymbol.swift
@@ -1,3 +1,47 @@
+public extension LSPDocumentSymbol
+{
+    var kindName: String
+    {
+        symbolKind?.name ?? "Unknown kind of symbol"
+    }
+}
+
+public extension LSPDocumentSymbol.SymbolKind
+{
+    var name: String
+    {
+        switch self
+        {
+        case .File: return "File"
+        case .Module: return "Module"
+        case .Namespace: return "Namespace"
+        case .Package: return "Package"
+        case .Class: return "Class"
+        case .Method: return "Method"
+        case .Property: return "Property"
+        case .Field: return "Field"
+        case .Constructor: return "Constructor"
+        case .Enum: return "Enum"
+        case .Interface: return "Interface"
+        case .Function: return "Function"
+        case .Variable: return "Variable"
+        case .Constant: return "Constant"
+        case .String: return "String"
+        case .Number: return "Number"
+        case .Boolean: return "Boolean"
+        case .Array: return "Array"
+        case .Object: return "Object"
+        case .Key: return "Key"
+        case .Null: return "Null"
+        case .EnumMember: return "EnumMember"
+        case .Struct: return "Struct"
+        case .Event: return "Event"
+        case .Operator: return "Operator"
+        case .TypeParameter: return "TypeParameter"
+        }
+    }
+}
+
 /**
  https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#documentSymbol
  */
