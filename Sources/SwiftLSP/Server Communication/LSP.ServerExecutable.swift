619e9354f0d0877168a70de5c438bd1b532ef335
Sebastian Fichtner <contact@flowtoolz.com>
2022-06-06 13:23:25 +0100

    Extract general class Executable to FoundationToolz

--- Sources/SwiftLSP/Server Communication/LSP.ServerExecutable.swift
+++ Sources/SwiftLSP/Server Communication/LSP.ServerExecutable.swift
@@ -1,158 +1,30 @@
-import Foundation
+import FoundationToolz
 import SwiftyToolz
 
 public extension LSP {
-    
-    class ServerExecutable {
+
+    /// This does not work in a sandboxed app!
+    class ServerExecutable: Executable {
         
         // MARK: - Life Cycle
         
-        public init(config: Configuration) throws {
-            guard FileManager.default.fileExists(atPath: config.path) else {
-                throw "Executable does not exist at given path \(config.path)"
-            }
+        public override init(config: Configuration) throws {
+            try super.init(config: config)
             
-            didSend = { _ in
-                log(warning: "\(Self.self) did send lsp packet, but handler has not been set")
-            }
-            
-            didSendError = { _ in
-                log(warning: "\(Self.self) did send error, but handler has not been set")
-            }
-            
-            didTerminate = {
-                log(warning: "\(Self.self) did terminate, but handler has not been set")
-            }
-            
-            try setupProcess(with: config)
-            setupInput()
-            setupOutput()
-            setupErrorOutput()
+            setupPacketOutput()
         }
-        
-        deinit { if isRunning { stop() } }
-        
-        // MARK: - LSP Packet Input
-        
-        private func setupInput() {
-            process.standardInput = inPipe
-        }
-        
-        public func receive(lspPacket: Data) {
-            guard isRunning else {
-                log(error: "\(Self.self) cannot receive LSP Packet while not running.")
-                return
-            }
-            
-            if lspPacket.isEmpty {
-                log(warning: "\(Self.self) received empty LSP Packet.")
-            }
-            
-            do {
-                if #available(OSX 10.15.4, *) {
-                    try inPipe.fileHandleForWriting.write(contentsOf: lspPacket)
-                } else {
-                    inPipe.fileHandleForWriting.write(lspPacket)
-                }
-            } catch { log(error) }
-        }
-        
-        private let inPipe = Pipe()
         
         // MARK: - LSP Packet Output
         
-        private func setupOutput() {
-            outPipe.fileHandleForReading.readabilityHandler = { [weak self] outHandle in
-                let serverOutput = outHandle.availableData
-                if serverOutput.count > 0 {
-                    self?.packetDetector.read(serverOutput)
-                }
-            }
-            
-            packetDetector.didDetect = { [weak self] packet in
-                self?.didSend(packet)
-            }
-            
-            process.standardOutput = outPipe
+        private func setupPacketOutput() {
+            didSendOutput = { [weak self] in self?.packetDetector.read($0) }
+            packetDetector.didDetect = { [weak self] in self?.didSend($0) }
         }
         
         private let packetDetector = LSP.PacketDetector()
         
-        public var didSend: (LSP.Packet) -> Void
-        private let outPipe = Pipe()
-        
-        // MARK: - Error Output
-        
-        private func setupErrorOutput() {
-            errorPipe.fileHandleForReading.readabilityHandler = { [weak self] errorHandle in
-                let errorData = errorHandle.availableData
-                if errorData.count > 0 { self?.didSendError(errorData) }
-            }
-            process.standardError = errorPipe
-        }
-        
-        public var didSendError: (Data) -> Void
-        private let errorPipe = Pipe()
-        
-        // MARK: - Process
-        
-        private func setupProcess(with config: Configuration) throws {
-            process.executableURL = URL(fileURLWithPath: config.path)
-            
-            let currentEnvironment = ProcessInfo.processInfo.environment
-            process.environment = currentEnvironment.merging(config.environment) { $1 }
-            
-            process.arguments = config.arguments
-            
-            process.terminationHandler = { [weak self] process in
-                log("\(Self.self) terminated. code: \(process.terminationReason.rawValue)")
-                self?.didTerminate()
-            }
-        }
-        
-        public var didTerminate: () -> Void
-        
-        public func run() {
-            guard process.executableURL != nil else {
-                log(error: "\(Self.self) has no valid executable set")
-                return
-            }
-            
-            guard !isRunning else {
-                log(warning: "\(Self.self) is already running.")
-                return
-            }
-            
-            do {
-                try process.run()
-            } catch {
-                log(error)
-            }
-        }
-        
-        public func stop() {
-            process.terminate()
-        }
-        
-        public var isRunning: Bool { process.isRunning }
-        
-        private let process = Process()
-        
-        // MARK: - Configuration
-        
-        public struct Configuration {
-            
-            public init(path: String,
-                        arguments: [String] = [],
-                        environment: [String : String] = [:]) {
-                self.path = path
-                self.arguments = arguments
-                self.environment = environment
-            }
-            
-            public var path: String
-            public var arguments: [String]
-            public var environment: [String: String]
+        public var didSend: (LSP.Packet) -> Void = { _ in
+            log(warning: "LSP server did send lsp packet, but handler has not been set")
         }
     }
 }
