4d143adadcc36fb50e0363d6b3f4e9d8d3f43466
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-29 12:27:48 +0200

    Add LSPRange containment check

--- Sources/SwiftLSP/Use Cases/Basic LSP Types/LSPLocation.swift
+++ Sources/SwiftLSP/Use Cases/Basic LSP Types/LSPLocation.swift
@@ -9,6 +9,30 @@ public struct LSPLocation: Codable
 
 public typealias LSPDocumentUri = String
 
+public extension LSPRange
+{
+    func contains(_ otherRange: LSPRange) -> Bool
+    {
+        if otherRange.start.line < start.line { return false }
+        if otherRange.start.line == start.line,
+           otherRange.start.character < start.character { return false }
+        
+        if otherRange.start.line > end.line { return false }
+        if otherRange.start.line == end.line,
+           otherRange.start.character > end.character { return false }
+        
+        if otherRange.end.line < start.line { return false }
+        if otherRange.end.line == start.line,
+           otherRange.end.character < start.character { return false }
+        
+        if otherRange.end.line > end.line { return false }
+        if otherRange.end.line == end.line,
+           otherRange.end.character > end.character { return false }
+        
+        return true
+    }
+}
+
 public struct LSPRange: Codable
 {
     /**
