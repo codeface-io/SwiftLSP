fad5d124a862e38aa071f8ed1aac275a6c579fb6
Sebastian Fichtner <contact@flowtoolz.com>
2023-01-05 19:15:29 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -13,11 +13,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/flowtoolz/FoundationToolz.git",
-            exact: "0.1.3"
+            exact: "0.1.6"
         ),
         .package(
             url: "https://github.com/flowtoolz/SwiftyToolz.git",
-            exact: "0.2.0"
+            exact: "0.2.2"
         )
     ],
     targets: [
